5fd2fa661292df4c556708ea6545e37899caa582
Hugo Thiolliere <Hugo Thiolliere>
2017-09-13 21:38:04 +0200

    main functionalities ok

--- p_dist/p_dist.js
+++ p_dist/p_dist.js
@@ -1,15 +1,54 @@
+var p = 1;
+var inpt;
 function setup() {
   createCanvas(300, 300);
-  createDiv("options");
-  slider = createSlider(0, 100, 50);
+
+  opt = createDiv("Options :");
+  slider = createSlider(0, 700, 50);
+  txt = createDiv(slider.value());
+  inpt = createInput();
+  par = createP("Use the slider to select the value of p. ");
+  slider.changed(emptyInpt);
+}
+function emptyInpt(){
+     inpt.value("");
+   }
+function computeLpNorm(x, y, p) {
+  if (p == 0) {
+    p = 0.001 ;
+  }
+  return pow ( pow(abs(x), p) + pow(abs(y), p), 1/p);
 }
 
 function draw() {
-  background(23, 178, 45,12);
-  fill(255);
-  line(.1*width, height/2 , .9*width, height /2);
-  line( width/2, .1*height, width/2 , .9*height); 
-  translate(width/2, height/2);
+  background(233);
 
-  ellipse(0, 0, slider.value(), slider.value());
+
+  line(.1*width, height/2, .9*width, height /2);
+  line( width/2, .1*height, width/2, .9*height); 
+  translate(width/2, height/2);
+  fill(23, 45, 123);
+  p = slider.value()/100;
+  if (inpt.value()) {
+    p = inpt.value();
+  }
+  txt.html(p);
+
+  for (var i = 0; i < 200; i++ ) {
+    for (var j =i; j< 200; j++ ) {
+      var clpn = computeLpNorm (i, j, p); 
+      if ( clpn <= 102 && clpn >= 80) {
+        ellipse(i, j, 2, 2);
+        ellipse(i, -j, 2, 2); 
+        ellipse(-i, -j, 2, 2); 
+        ellipse(-i, j, 2, 2);
+        ellipse(j, i, 2, 2);
+        ellipse(j, -i, 2, 2); 
+        ellipse(-j, -i, 2, 2); 
+        ellipse(-j, i, 2, 2);
+      }
+    }
+  }
+  fill(255);
+  //  ellipse(0, 0, slider.value(), slider.value());
 }
